c02f55e22e4da93cd8316838f8b0e1b43dc4f6f0
Kara Parikh <karaaparikh@gmail.com>
2025-05-03 17:44:10 -0400

    Initial commit (part 2)

---
+++ EchoLearn/EchoLearn/EchoLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoLearnApp.swift
+//  EchoLearn
+//
+//  Created by Kirsten Morgan Parikh on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct EchoLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EchoLearn/EchoLearnTests/EchoLearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoLearnTests.swift
+//  EchoLearnTests
+//
+//  Created by Kirsten Morgan Parikh on 5/3/25.
+//
+
+import Testing
+@testable import EchoLearn
+
+struct EchoLearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
