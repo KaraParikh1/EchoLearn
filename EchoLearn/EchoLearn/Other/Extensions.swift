357fa2fd6b87fe0060c35117eddc0945d9ca07b3
Kara Parikh <karaaparikh@gmail.com>
2025-05-05 15:37:05 -0400

    Added Firebase Backend for signin and ugly signin/register pages.

---
+++ EchoLearn/EchoLearn/Other/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  EchoLearn
+//
+//  Created by Kirsten Morgan Parikh on 5/4/25.
+//
+
+import Foundation
---
+++ EchoLearn/EchoLearn/Views/ELButton.swift
@@ -0,0 +1,18 @@
+//
+//  ELButton.swift
+//  EchoLearn
+//
+//  Created by Kirsten Morgan Parikh on 5/4/25.
+//
+
+import SwiftUI
+
+struct ELButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ELButton()
+}
